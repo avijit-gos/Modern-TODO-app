d9dfb26a779782bdb80b1dbb86399c1d59aa0561
newbieAt69 <anikeshdey2020@gmail.com>
2023-03-14 12:17:12 +0530

    Chat backend created

--- frontend/src/Component/Headers/MessageHeader/MessageHeader.jsx
+++ frontend/src/Component/Headers/MessageHeader/MessageHeader.jsx
@@ -7,23 +7,133 @@ import {
   MenuButton,
   MenuList,
   MenuItem,
+  Input,
 } from "@chakra-ui/react";
 import { IoArrowBack } from "react-icons/io5";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { CgMoreO } from "react-icons/cg";
+import ModalComp from "../../ModalComp/ModalComp";
+import { TbUserSearch } from "react-icons/tb";
+import InputComp from "../../InputComp/InputComp";
+import UserList from "../../UserList/UserList";
+import UserListLoader from "../../SkeletonLoader/UserListLoader";
 
 const MessageHeader = ({ title }) => {
   const navigate = useNavigate();
+  const [openSingleChatModal, setOpenSingleChatModal] = React.useState(false);
+  const [openGroupChatModal, setOpenGroupChatModal] = React.useState(false);
+  const [searchText, setSearchText] = React.useState("");
+  const [groupName, setGroupName] = React.useState("");
+  const [members, setMembers] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [userList, setUserList] = React.useState([]);
+  const [msg, setMsg] = React.useState("");
+  const [disable, setDisable] = React.useState(true);
 
   const backToPrevPage = () => {
     navigate(-1);
   };
 
+  // *** Close modal
+  const onClose = () => {
+    setOpenSingleChatModal(false);
+    setOpenGroupChatModal(false);
+    setGroupName("");
+    setSearchText("");
+    setMembers([]);
+    setUserList([]);
+  };
+
+  // *** Handle open single chat modal
+  const handleOpenSingleChatModal = () => {
+    setOpenSingleChatModal(true);
+  };
+
+  const handleOpenGroupChatModal = () => {
+    setOpenGroupChatModal(true);
+  };
+
+  // Search user
+  React.useEffect(() => {
+    if (searchText.trim()) {
+      setDisable(false);
+      setIsLoading(true);
+      var myHeaders = new Headers();
+      myHeaders.append("x-access-token", localStorage.getItem("token"));
+      myHeaders.append("Content-Type", "application/json");
+      var requestOptions = {
+        method: "GET",
+        headers: myHeaders,
+        redirect: "follow",
+      };
+      fetch(
+        `${process.env.REACT_APP_LINK}user/find/user?key=${searchText}`,
+        requestOptions
+      )
+        .then((response) => response.json())
+        .then((result) => {
+          console.log(result);
+          setUserList(result);
+          setIsLoading(false);
+        })
+        .catch((error) => console.log("error", error));
+    } else {
+      // setDisable(true);
+      setMsg("No recent searches.");
+    }
+  }, [searchText]);
+
   return (
     <Box className='header_navbar_container'>
+      {openSingleChatModal && (
+        <ModalComp
+          isOpen={openSingleChatModal}
+          onClose={onClose}
+          title={"Create a new chat"}
+          body={
+            <Box className='single_create_chat_modal'>
+              <Box className='user_search_section'>
+                <TbUserSearch className='user_search_icon' />
+                <InputComp
+                  type='search'
+                  placaeholder='Search user to start chatting...'
+                  className='search_input'
+                  value={searchText}
+                  handleChange={(e) => setSearchText(e.target.value)}
+                />
+              </Box>
+              <Box className='modal_user_search_result_section'>
+                {disable ? (
+                  <Box className='empty_modal_user_list'>Start searching</Box>
+                ) : (
+                  <Box className='modal_user_list'>
+                    {isLoading ? (
+                      <UserListLoader />
+                    ) : (
+                      <React.Fragment>
+                        {(userList || []).length > 0 ? (
+                          <React.Fragment>
+                            {userList.map((user) => (
+                              <UserList key={user._id} userData={user} />
+                            ))}
+                          </React.Fragment>
+                        ) : (
+                          <Box className='empty_search_result'>
+                            No user found
+                          </Box>
+                        )}
+                      </React.Fragment>
+                    )}
+                  </Box>
+                )}
+              </Box>
+            </Box>
+          }
+        />
+      )}
       <Box className='header_box'>
-        <Button className='back_btn'>
+        <Button className='back_btn' onClick={backToPrevPage}>
           <IoArrowBack />
         </Button>
         <span className='header_title'>{title}</span>
@@ -34,8 +144,16 @@ const MessageHeader = ({ title }) => {
           <CgMoreO className='header_menu_icon' />
         </MenuButton>
         <MenuList>
-          <MenuItem className='header_menu_item'>Create single chat</MenuItem>
-          <MenuItem className='header_menu_item'>Create a group chat</MenuItem>
+          <MenuItem
+            className='header_menu_item'
+            onClick={handleOpenSingleChatModal}>
+            Create single chat
+          </MenuItem>
+          <MenuItem
+            className='header_menu_item'
+            onClick={handleOpenGroupChatModal}>
+            Create a group chat
+          </MenuItem>
         </MenuList>
       </Menu>
     </Box>
---
+++ frontend/src/Component/SkeletonLoader/UserListLoader.jsx
@@ -0,0 +1,41 @@
+/** @format */
+
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+const UserListLoader = () => {
+  return (
+    <React.Fragment>
+      <Box className='user_loader_list_card'>
+        <Box className='user_loader_list_img skeleton'></Box>
+        <Box className='user_loader_list_name skeleton'></Box>
+      </Box>
+      <Box className='user_loader_list_card'>
+        <Box className='user_loader_list_img skeleton'></Box>
+        <Box className='user_loader_list_name skeleton'></Box>
+      </Box>
+      <Box className='user_loader_list_card'>
+        <Box className='user_loader_list_img skeleton'></Box>
+        <Box className='user_loader_list_name skeleton'></Box>
+      </Box>
+      <Box className='user_loader_list_card'>
+        <Box className='user_loader_list_img skeleton'></Box>
+        <Box className='user_loader_list_name skeleton'></Box>
+      </Box>
+      <Box className='user_loader_list_card'>
+        <Box className='user_loader_list_img skeleton'></Box>
+        <Box className='user_loader_list_name skeleton'></Box>
+      </Box>
+      <Box className='user_loader_list_card'>
+        <Box className='user_loader_list_img skeleton'></Box>
+        <Box className='user_loader_list_name skeleton'></Box>
+      </Box>
+      <Box className='user_loader_list_card'>
+        <Box className='user_loader_list_img skeleton'></Box>
+        <Box className='user_loader_list_name skeleton'></Box>
+      </Box>
+    </React.Fragment>
+  );
+};
+
+export default UserListLoader;
